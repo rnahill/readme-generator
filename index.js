0fdfce39e230175bb6ef9618eea50a38c86742df
rnahill <rei333lly@gmail.com>
2023-11-22 15:28:34 -0500

    added rough draft of questions

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    "What is the title of your project?",
+    "Include a description of your project:",
+    "Create a Table of Contents:",
+    "How can a user install your application?",
+    "How do you use the application?",
+    "What License did you use?",
+    "Who contributed to the project?",
+    "List any tests here:",
+    "What is your Github username?",
+    "What contact information can a user use to contact you?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
