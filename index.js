8442cefc71a4240944cb93be21b8d2ee7f474a02
rnahill <rei333lly@gmail.com>
2023-11-26 11:23:09 -0500

    added questions as objects

--- index.js
+++ index.js
@@ -1,20 +1,71 @@
 // TODO: Include packages needed for this application
 
 const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    "What is the title of your project?",
-    "Include a description of your project:",
-    "Create a Table of Contents:",
-    "How can a user install your application?",
-    "How do you use the application?",
-    "What License did you use?",
-    "Who contributed to the project?",
-    "List any tests here:",
-    "What is your Github username?",
-    "What contact information can a user use to contact you?"
+const questions = [ 
+{
+    type: "input",
+    question:  "What is the title of your project?",
+    name: "title"
+},
+
+{
+    type: "input",
+    question:  "Include a description of your project:",
+    name: "description"
+},
+
+{
+    type: "input",
+    question:  "If applicable, provide installation intructions for your application:",
+    name: "installation"
+},
+
+{
+    type: "input",
+    question:  "Include usage instructions for your application:",
+    name: "usage"
+},
+
+{
+    type: "list",
+    question:   "Which License did you use?",
+    choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense', 'Other License'],
+    name: "license"
+},
+
+{
+    type: "input",
+    question:   "Include anyone who contributed to the project, if any.",
+    name: "contributors"
+},
+
+{
+    type: "input",
+    question:   "If applicable, give instructions on how to test your application:",
+    name: "tests"
+},
+
+{
+    type: "input",
+    question:    "What is your Github username? (No @ needed)",
+    name: "username"
+},
+
+{
+    type: "input",
+    question:    "What email address can a user use to contact you?",
+    name: "contact"
+},
+
 ];
+
+
+// need table of contents
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
